c8c367414f9ffa579946fa6ed69984e7593d0d32
padi-dev-tuantv <tuantv@paditech.com>
2023-03-31 14:47:45 +0700

    update display

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
